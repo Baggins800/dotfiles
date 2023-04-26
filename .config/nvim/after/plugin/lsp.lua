3e3fac2f3739ca04e7c5519a73267bc3c4b312bb
Ruan Luies <omega@live.co.za>
2023-04-26 12:49:20 +0200

    []p is closer to my hand.

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -10,9 +10,9 @@ lsp.ensure_installed({
 local cmp = require("cmp")
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mapping = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({select = true}),
+  ['<C-]>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-[>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-p>'] = cmp.mapping.confirm({select = true}),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 
