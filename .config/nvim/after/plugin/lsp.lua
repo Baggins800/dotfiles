4350b647dd631f457c383cab2c02d1bfd51d3e51
Ruan Luies <omega@live.co.za>
2025-07-31 21:34:57 +0200

    fix: remove some old hyprland settings and lsps.

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -2,10 +2,8 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 lsp.ensure_installed({
   "rust_analyzer",
-  "eslint",
   "clangd",
   "cmake",
-  "intelephense"
 })
 
 local cmp = require("cmp")
